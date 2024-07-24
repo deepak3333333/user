f262862cc4b518033eb192c4842d4ab649d2f7f1
deepak3333333 <deepak20wj1a6713@gmail.com>
2024-07-24 21:37:05 +0530

    second commit

--- controllers/url.js
+++ controllers/url.js
@@ -1,6 +1,7 @@
 const { mongoose } = require('mongoose')
 const shortid=require('shortid')
 const URL = require('../models/url')
+
 async function handleurl(req,res){
     console.log(req.body)
     body=req.body
@@ -17,5 +18,20 @@ async function handleurl(req,res){
     })
 return res.json({message:"hello world",shortId})
 }
+ async function findingurl(req,res){
+    const shortId=req.params.shorturl
+    const entry=await URL.findOneAndUpdate({shortid:shortId},{
+        
+    })
+    if(entry){
+         return res.redirect(entry.redirectedUrl)
+    
+}
+   
+    
+    
+    
 
-module.exports = {handleurl}
+
+}
+module.exports = {handleurl,findingurl}
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,8 @@
 const express=require("express")
 const router=express.Router()
-const {handleurl}=require("../controllers/url")
+const {handleurl,findingurl}=require("../controllers/url")
 router.post("/",handleurl)
+router.get("/:shorturl",findingurl)
 
 
 
